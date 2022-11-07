971d2eb94f47750b61d0a4c85d13c426982bb775
noor <noorridha028@gmail.com>
2022-11-07 21:35:56 +0300

    finally doooooone

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+ let form = document.querySelector("form")
+ form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDoList(e.target.newtask.value)
+    form.reset()
+  } )
+})
+
+function handleToDoList(todo){
+ let  p= document.createElement('p')
+ let bttn= document.createElement('button')
+ bttn.addEventListener('click', handleDelet)
+ bttn.textContent='remove'
+ p.textContent = ` ${todo}  `
+ p.appendChild(bttn)
+ console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+function handleDelet(e){
+  e.target.parentNode.remove()
+}
